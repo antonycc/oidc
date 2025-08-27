e00c256882ef492da94c83b3f827d34cf124eaab
Antony Cartwright <antonyccartwright@gmail.com>
2025-08-27 18:43:34 +0100

    Remove cloudFrontHostedZoneId from App.java

--- infra/main/java/com/antonycc/oidc/App.java
+++ infra/main/java/com/antonycc/oidc/App.java
@@ -17,7 +17,6 @@ public class App {
     String authCertificateArn =
           System.getenv()
                   .getOrDefault("AUTH_CERTIFICATE_ARN", "arn:aws:acm:us-east-1:123456789012:certificate/xyz");
-    String cloudFrontHostedZoneId = System.getenv().getOrDefault("CLOUDFRONT_HOSTED_ZONE_ID", "Z2FDTNDATAQYW2");
 
     Environment env =
         Environment.builder()
@@ -67,7 +66,6 @@ public class App {
                 .authCertificateArn(authCertificateArn)
                 .hostedZoneName(hostedZoneName)
                 .hostedZoneId(hostedZoneId)
-                .cloudFrontHostedZoneId(cloudFrontHostedZoneId)
                 .build());
 
     providerStack.addDependency(observabilityStack);
